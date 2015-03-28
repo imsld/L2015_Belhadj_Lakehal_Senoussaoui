7296b852bb4ad40b9e5eb51de2979e7e9530f29a
imsld <ismailinginf@yahoo.fr>
2015-03-28 23:28:14 +0100

    Version Stable. Travail à faire : Terminer le intetnt de l'historique

---
+++ Android_L2015_BLS/src/com/pfe/bls/HistoryFileManager.java
@@ -0,0 +1,338 @@
+package com.pfe.bls;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import android.annotation.SuppressLint;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+@SuppressLint("SdCardPath")
+public class HistoryFileManager implements Parcelable {
+
+	static final String KEY_RACINE = "Applications";
+	static final String KEY_ID = "ID";
+	static final String KEY_APPLICATION = "Application";
+	static final String KEY_DATE_CREATION = "Date_creation";
+	static final String KEY_NOM_APPLICATION = "Nom";
+	static final String KEY_VERSION_SDK = "Version_SDK";
+	static final String KEY_DESCRIPTION = "Description";
+	static final String KEY_CALCULATRICE = "Calculatrice";
+	static final String KEY_CALENDRIER = "Calendrier";
+	static final String KEY_MESSAGERIE = "Messagerie";
+	static final String KEY_REPARTOIRE = "Repartoire";
+	static final String KEY_LOCALISATION = "Localisation";
+	static final String KEY_APPEL = "Appel";
+	static final String KEY_CREDIT = "Credit";
+
+	private String filePath = "/data/data/com.pfe.bls/historique.xml";
+	private File xmlFile = null;
+
+	public Document doc = null;
+	private DocumentBuilder dBuilder;
+	private DocumentBuilderFactory dbFactory;
+
+	private int CurrentID = -1;
+
+	public ArrayList<HashMap<String, String>> menuItems;
+
+	public static final Creator<HistoryFileManager> CREATOR = new Creator<HistoryFileManager>() {
+		@Override
+		public HistoryFileManager createFromParcel(Parcel source) {
+			HistoryFileManager mHistFile = new HistoryFileManager();
+			mHistFile.filePath = source.readString();
+			mHistFile.menuItems = (ArrayList<HashMap<String, String>>) source
+					.readSerializable();
+			return mHistFile;
+		}
+
+		@Override
+		public HistoryFileManager[] newArray(int size) {
+			return new HistoryFileManager[size];
+		}
+	};
+
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel arg0, int arg1) {
+		// TODO Auto-generated method stub
+		arg0.writeString(filePath);
+		arg0.writeSerializable(menuItems);
+	}
+
+	private boolean checkHistoryFile() {
+		File xmlFileTest = new File(filePath);
+		if (xmlFileTest.isFile())
+			return true;
+		else
+			return false;
+	}
+
+	private void initXMLFile() {
+		if (xmlFile == null)
+			xmlFile = new File(filePath);
+	}
+
+	private void createXMLDocument() {
+
+		dbFactory = DocumentBuilderFactory.newInstance();
+		try {
+
+			dBuilder = dbFactory.newDocumentBuilder();
+			doc = dBuilder.newDocument();
+			Element racine = doc.createElement(KEY_RACINE);
+			doc.appendChild(racine);
+
+			writeInFile();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void writeInFile() {
+		// for output to file, console
+		TransformerFactory transformerFactory = TransformerFactory
+				.newInstance();
+		Transformer transformer;
+
+		try {
+			transformer = transformerFactory.newTransformer();
+			// for pretty print
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			DOMSource source = new DOMSource(doc);
+
+			// write to console or file
+			StreamResult console = new StreamResult(System.out);
+			StreamResult file = new StreamResult(new File(filePath));
+
+			// write data
+			transformer.transform(source, console);
+			transformer.transform(source, file);
+
+			xmlFile = new File(filePath);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void ParseDocument() {
+		if (doc == null) {
+			dbFactory = DocumentBuilderFactory.newInstance();
+			try {
+				dBuilder = dbFactory.newDocumentBuilder();
+				doc = dBuilder.parse(xmlFile);
+				doc.getDocumentElement().normalize();
+
+				doc.getDocumentElement().normalize();
+				NodeList nList = doc.getElementsByTagName("Application");
+				Element nNode = (Element) nList.item(nList.getLength());
+				CurrentID = Integer.parseInt(nNode.getAttribute(KEY_ID));
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private int getID() {
+		int maxID = 0;
+
+		if (getCountApplication() == 0)
+			return maxID+1;
+		else {
+			doc.getDocumentElement().normalize();
+			NodeList nList = doc.getElementsByTagName("Application");
+			for (int i = 0; i < nList.getLength(); i++) {
+				Element nNode = (Element) nList.item(i);
+				int localID = Integer.parseInt(nNode.getAttribute(KEY_ID));
+				if (localID > maxID)
+					maxID = localID;
+			}
+			return maxID + 1;
+		}
+	}
+
+	public void initXMLDocument() {
+		if (!checkHistoryFile()) {
+			createXMLDocument();
+		} else
+			initXMLFile();
+
+		ParseDocument();
+	}
+
+	public int getCurrentID() {
+		return CurrentID;
+	}
+
+	public int getCountApplication() {
+		Element item = doc.getDocumentElement();
+		NodeList listApplication = item.getElementsByTagName("Application");
+		return listApplication.getLength();
+	}
+
+	public void addApplication(List<String> listInfo) {
+		doc.getDocumentElement().normalize();
+
+		CurrentID = getID();
+		
+		Element racine = doc.getDocumentElement();
+		Element appliElement = doc.createElement(KEY_APPLICATION);
+		racine.appendChild(appliElement);
+
+		
+		appliElement.setAttribute(KEY_ID, Integer.toString(CurrentID));
+
+		Element newChild_0 = doc.createElement(KEY_DATE_CREATION);
+		newChild_0.setTextContent(listInfo.get(0));
+		appliElement.appendChild(newChild_0);
+
+		Element newChild_1 = doc.createElement(KEY_NOM_APPLICATION);
+		newChild_1.setTextContent(listInfo.get(1));
+		appliElement.appendChild(newChild_1);
+
+		Element newChild_2 = doc.createElement(KEY_VERSION_SDK);
+		newChild_2.setTextContent(listInfo.get(2));
+		appliElement.appendChild(newChild_2);
+
+		Element newChild_3 = doc.createElement(KEY_DESCRIPTION);
+		newChild_3.setTextContent(listInfo.get(3));
+		appliElement.appendChild(newChild_3);
+
+		Element newChild_4 = doc.createElement(KEY_CALCULATRICE);
+		newChild_4.setTextContent(listInfo.get(4));
+		appliElement.appendChild(newChild_4);
+
+		Element newChild_5 = doc.createElement(KEY_CALENDRIER);
+		newChild_5.setTextContent(listInfo.get(5));
+		appliElement.appendChild(newChild_5);
+
+		Element newChild_6 = doc.createElement(KEY_MESSAGERIE);
+		newChild_6.setTextContent(listInfo.get(6));
+		appliElement.appendChild(newChild_6);
+
+		Element newChild_7 = doc.createElement(KEY_REPARTOIRE);
+		newChild_7.setTextContent(listInfo.get(7));
+		appliElement.appendChild(newChild_7);
+
+		Element newChild_8 = doc.createElement(KEY_LOCALISATION);
+		newChild_8.setTextContent(listInfo.get(8));
+		appliElement.appendChild(newChild_8);
+
+		Element newChild_9 = doc.createElement(KEY_APPEL);
+		newChild_9.setTextContent(listInfo.get(9));
+		appliElement.appendChild(newChild_9);
+
+		Element newChild_10 = doc.createElement(KEY_CREDIT);
+		newChild_10.setTextContent(listInfo.get(10));
+		appliElement.appendChild(newChild_10);
+
+		doc.getDocumentElement().normalize();
+		writeInFile();
+	}
+
+	public void deleteCurentApplication(){
+		
+	}
+	public List<String> getCurrentApplication() {
+		List<String> listInfo = new ArrayList<String>();
+		doc.getDocumentElement().normalize();
+		NodeList nList = doc.getElementsByTagName("Application");
+		for (int i = 0; i < nList.getLength(); i++) {
+			Element nNode = (Element) nList.item(i);
+			if (Integer.parseInt(nNode.getAttribute(KEY_ID)) == CurrentID) {
+				listInfo.add(nNode.getElementsByTagName(KEY_DATE_CREATION)
+						.item(0).getTextContent());
+				listInfo.add(nNode.getElementsByTagName(KEY_NOM_APPLICATION)
+						.item(0).getTextContent());
+				listInfo.add(nNode.getElementsByTagName(KEY_VERSION_SDK)
+						.item(0).getTextContent());
+				listInfo.add(nNode.getElementsByTagName(KEY_DESCRIPTION)
+						.item(0).getTextContent());
+				listInfo.add(nNode.getElementsByTagName(KEY_CALCULATRICE)
+						.item(0).getTextContent());
+				listInfo.add(nNode.getElementsByTagName(KEY_CALENDRIER).item(0)
+						.getTextContent());
+				listInfo.add(nNode.getElementsByTagName(KEY_MESSAGERIE).item(0)
+						.getTextContent());
+				listInfo.add(nNode.getElementsByTagName(KEY_REPARTOIRE).item(0)
+						.getTextContent());
+				listInfo.add(nNode.getElementsByTagName(KEY_LOCALISATION)
+						.item(0).getTextContent());
+				listInfo.add(nNode.getElementsByTagName(KEY_APPEL).item(0)
+						.getTextContent());
+				listInfo.add(nNode.getElementsByTagName(KEY_CREDIT).item(0)
+						.getTextContent());
+			}
+		}
+		return listInfo;
+	}
+
+	public void putMenuItems() {
+		menuItems = new ArrayList<HashMap<String, String>>();
+		doc.getDocumentElement().normalize();
+		NodeList nList = doc.getElementsByTagName("Application");
+
+		for (int i = 0; i < nList.getLength(); i++) {
+			// creating new HashMap
+			HashMap<String, String> map = new HashMap<String, String>();
+			Element nNode = (Element) nList.item(i);
+			map.put(KEY_ID, nNode.getAttribute(KEY_ID));
+			map.put(KEY_DATE_CREATION,
+					nNode.getElementsByTagName(KEY_DATE_CREATION).item(0)
+							.getTextContent());
+			map.put(KEY_NOM_APPLICATION,
+					nNode.getElementsByTagName(KEY_NOM_APPLICATION).item(0)
+							.getTextContent());
+			map.put(KEY_VERSION_SDK, nNode
+					.getElementsByTagName(KEY_VERSION_SDK).item(0)
+					.getTextContent());
+			map.put(KEY_DESCRIPTION, nNode
+					.getElementsByTagName(KEY_DESCRIPTION).item(0)
+					.getTextContent());
+			map.put(KEY_CALCULATRICE,
+					nNode.getElementsByTagName(KEY_CALCULATRICE).item(0)
+							.getTextContent());
+			map.put(KEY_CALENDRIER, nNode.getElementsByTagName(KEY_CALENDRIER)
+					.item(0).getTextContent());
+			map.put(KEY_MESSAGERIE, nNode.getElementsByTagName(KEY_MESSAGERIE)
+					.item(0).getTextContent());
+			map.put(KEY_REPARTOIRE, nNode.getElementsByTagName(KEY_REPARTOIRE)
+					.item(0).getTextContent());
+			map.put(KEY_LOCALISATION,
+					nNode.getElementsByTagName(KEY_LOCALISATION).item(0)
+							.getTextContent());
+			map.put(KEY_APPEL, nNode.getElementsByTagName(KEY_APPEL).item(0)
+					.getTextContent());
+			map.put(KEY_CREDIT, nNode.getElementsByTagName(KEY_CREDIT).item(0)
+					.getTextContent());
+			// adding HashList to ArrayList
+			menuItems.add(map);
+		}
+	}
+}
--- Android_L2015_BLS/src/com/pfe/bls/MainActivity.java
+++ Android_L2015_BLS/src/com/pfe/bls/MainActivity.java
@@ -1,7 +1,18 @@
-package com.example.android;
+package com.pfe.bls;
 
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pfe.bls.R;
+import com.pfe.bls.HistoriqueActivity;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,25 +21,30 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+@SuppressLint("SdCardPath")
 public class MainActivity extends Activity {
 
-	HistoryFileManager HistFile;
-	
+	public final static String PAR_KEY = "com.pfe.bls.par";
+
+	HistoryFileManager HistFile = new HistoryFileManager();
+
 	Button btn_newApp;
 	Button btn_clearApp;
 	Button btn_HistApp;
 	Button btn_saveApp;
 	Button btn_createApp;
-	
+
 	EditText edit_AppName;
 	EditText edit_AppDescr;
-	
+
+	RadioGroup rb_Group_API;
 	RadioButton rb_ApiLocal;
 	RadioButton rb_Api18;
 	RadioButton rb_Api19;
 	RadioButton rb_Api20;
-	
+
 	CheckBox cb_Calc;
 	CheckBox cb_Cal;
 	CheckBox cb_Msg;
@@ -39,97 +55,154 @@ public class MainActivity extends Activity {
 
 	String myVersion;
 	int sdkVersion;
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		// associate the button from interface to code
 		initInterface();
 		addButtonClickListner();
-		
-		//Check history file if it exist
-		HistFile = new HistoryFileManager();
-		if (!HistFile.checkHistoryFile())
+		HistFile.initXMLDocument();
+		if (HistFile.getCountApplication() == 0)
 			btn_HistApp.setEnabled(false);
+		else
+			btn_HistApp.setEnabled(true);
 	}
 
-	private CharSequence getAPIVersion() {
-		//Getting device OS Version
-		myVersion = android.os.Build.VERSION.RELEASE; // e.g. myVersion := "1.6"
-		sdkVersion = android.os.Build.VERSION.SDK_INT; // e.g. sdkVersion := 8; 				
-		return "API "+ sdkVersion + ", version "+ myVersion +" (local API)";
-	}
-	
 	private void addButtonClickListner() {
-		
 		btn_newApp.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
-				//Getting device OS Version
+				edit_AppName.setText("");
+				edit_AppDescr.setText("");
 				rb_ApiLocal.setText(getAPIVersion());
 				rb_ApiLocal.setActivated(true);
-			}			
+				cb_Calc.setChecked(false);
+				cb_Cal.setChecked(false);
+				cb_Msg.setChecked(false);
+				cb_Rep.setChecked(false);
+				cb_GeoLoc.setChecked(false);
+				cb_Appel.setChecked(false);
+				cb_Credit.setChecked(false);
+			}
 		});
-		
+
 		btn_clearApp.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
-				//Getting device OS Version
+				// Getting device OS Version
 				rb_ApiLocal.setText(getAPIVersion());
 				rb_ApiLocal.setActivated(true);
 			}
 		});
-		
+
 		btn_HistApp.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
+				Intent myintent = new Intent(v.getContext(),
+						HistoriqueActivity.class);
+				Bundle mBundle = new Bundle();
+
+				HistFile.putMenuItems();
+				mBundle.putParcelable(PAR_KEY, HistFile);
+				myintent.putExtras(mBundle);
+				startActivity(myintent);
+				Log.i("ffffffff","aaaaaaaaaaaa");
 			}
 		});
-		
+
 		btn_createApp.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
 			}
 		});
-		
+
 		btn_saveApp.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
-				if (!HistFile.checkHistoryFile()){
-					HistFile.createEmptyRacine();;
-				}
-				else{
-					HistFile.createEmptyRacine();;
-				}					
+				List<String> listInfo = new ArrayList<String>();
+				String dateCreation, heureCreation;
+				dateCreation = new SimpleDateFormat("dd/MM/yyyy")
+				.format(System.currentTimeMillis());
+				heureCreation =  new SimpleDateFormat("HH:mm:ss")
+						.format(System.currentTimeMillis());
+
+				listInfo.add(dateCreation+","+ heureCreation);
+
+				listInfo.add(edit_AppName.getText().toString());
+
+				if (rb_ApiLocal.isChecked())
+					listInfo.add(Integer.toString(sdkVersion));
+				if (rb_Api18.isChecked())
+					listInfo.add("18");
+				if (rb_Api19.isChecked())
+					listInfo.add("19");
+				if (rb_Api20.isChecked())
+					listInfo.add("20");
+
+				listInfo.add(edit_AppDescr.getText().toString());
+
+				if (cb_Calc.isChecked())
+					listInfo.add("Cal");
+				else
+					listInfo.add("0");
+
+				if (cb_Cal.isChecked())
+					listInfo.add("Calc");
+				else
+					listInfo.add("0");
+
+				if (cb_Msg.isChecked())
+					listInfo.add("Msg");
+				else
+					listInfo.add("0");
+
+				if (cb_Rep.isChecked())
+					listInfo.add("Rep");
+				else
+					listInfo.add("0");
+
+				if (cb_GeoLoc.isChecked())
+					listInfo.add("GeoLoc");
+				else
+					listInfo.add("0");
+
+				if (cb_Appel.isChecked())
+					listInfo.add("Appel");
+				else
+					listInfo.add("0");
+
+				if (cb_Credit.isChecked())
+					listInfo.add("Credit");
+				else
+					listInfo.add("0");
+
+				HistFile.addApplication(listInfo);
+
+				if (HistFile.getCountApplication() == 0)
+					btn_HistApp.setEnabled(false);
+				else
+					btn_HistApp.setEnabled(true);
 			}
 		});
 	}
 
 	private void initInterface() {
-		/*Ici vousdevez faire toute les associations entre l'interface 
-		 * et le code cource
-		 * */
 		btn_newApp = (Button) findViewById(R.id.btnNewApplication);
 		btn_clearApp = (Button) findViewById(R.id.btnClearFields);
 		btn_HistApp = (Button) findViewById(R.id.btnShowHistory);
 		btn_saveApp = (Button) findViewById(R.id.btnSaveField);
 		btn_createApp = (Button) findViewById(R.id.btnCreatApplication);
-		
+
 		edit_AppName = (EditText) findViewById(R.id.editTextAppName);
 		edit_AppDescr = (EditText) findViewById(R.id.editTextAppDescr);
-		
+
+		rb_Group_API = (RadioGroup) findViewById(R.id.radioGroupAPI);
 		rb_ApiLocal = (RadioButton) findViewById(R.id.radioApiLocal);
 		rb_Api18 = (RadioButton) findViewById(R.id.radioApi18);
 		rb_Api19 = (RadioButton) findViewById(R.id.radioApi19);
 		rb_Api20 = (RadioButton) findViewById(R.id.radioApi20);
-		
+
 		cb_Calc = (CheckBox) findViewById(R.id.checkBoxCalc);
 		cb_Cal = (CheckBox) findViewById(R.id.checkBoxCal);
 		cb_Msg = (CheckBox) findViewById(R.id.checkBoxMsg);
@@ -138,7 +211,7 @@ public class MainActivity extends Activity {
 		cb_Appel = (CheckBox) findViewById(R.id.checkBoxAppel);
 		cb_Credit = (CheckBox) findViewById(R.id.checkBoxCredi);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -157,4 +230,12 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	private CharSequence getAPIVersion() {
+		// Getting device OS Version
+		myVersion = android.os.Build.VERSION.RELEASE; // e.g. myVersion := "1.6"
+		sdkVersion = android.os.Build.VERSION.SDK_INT; // e.g. sdkVersion := 8;
+		return "API " + sdkVersion + ", version " + myVersion + " (local API)";
+	}
+
 }
